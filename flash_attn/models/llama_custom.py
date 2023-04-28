7e70f09c15e77377834f38dabb25af4ab0d83895
koba <kobaqhat@gmail.com>
2023-04-28 12:30:14 +0200

    more llamafy

--- flash_attn/models/llama_custom.py
+++ flash_attn/models/llama_custom.py
@@ -55,23 +55,23 @@ def remap_state_dict_meta_llama(state_dict, config):
 
     # LayerNorm
     def key_mapping_ln(key):
-        key = re.sub(r'^transformer.norm.', r'transformer.ln_f.', key)
-        key = re.sub(r'^transformer.layers.(\d+).input_layernorm.', r'transformer.layers.\1.norm1.', key)
-        key = re.sub(r'^transformer.layers.(\d+).post_attention_layernorm.', r'transformer.layers.\1.norm2.', key)
+        key = re.sub(r'^model.norm.', r'model.ln_f.', key)
+        key = re.sub(r'^model.layers.(\d+).input_layernorm.', r'model.layers.\1.norm1.', key)
+        key = re.sub(r'^model.layers.(\d+).post_attention_layernorm.', r'model.layers.\1.norm2.', key)
         return key
 
     state_dict = OrderedDict((key_mapping_ln(k), v) for k, v in state_dict.items())
 
     # MLP
     for l in range(config.num_hidden_layers):
-        w1 = state_dict.pop(f'transformer.layers.{l}.mlp.gate_proj.weight')
-        w3 = state_dict.pop(f'transformer.layers.{l}.mlp.up_proj.weight')
+        w1 = state_dict.pop(f'model.layers.{l}.mlp.gate_proj.weight')
+        w3 = state_dict.pop(f'model.layers.{l}.mlp.up_proj.weight')
         # Our ordering is different
-        state_dict[f'transformer.layers.{l}.mlp.fc1.weight'] = torch.cat([w3, w1], dim=0)
+        state_dict[f'model.layers.{l}.mlp.fc1.weight'] = torch.cat([w3, w1], dim=0)
 
     def key_mapping_mlp(key):
-        return re.sub(r'^transformer.layers.(\d+).mlp.down_proj.',
-                      r'transformer.layers.\1.mlp.fc2.', key)
+        return re.sub(r'^model.layers.(\d+).mlp.down_proj.',
+                      r'model.layers.\1.mlp.fc2.', key)
 
     state_dict = OrderedDict((key_mapping_mlp(k), v) for k, v in state_dict.items())
     n_heads = config.num_attention_heads
@@ -83,24 +83,24 @@ def remap_state_dict_meta_llama(state_dict, config):
         return w.view(n_heads, 2, dim // n_heads // 2, dim).transpose(2, 1).reshape(dim, dim)
 
     for l in range(config.num_hidden_layers):
-        Wq = state_dict.pop(f'transformer.layers.{l}.self_attn.q_proj.weight')
-        Wk = state_dict.pop(f'transformer.layers.{l}.self_attn.k_proj.weight')
+        Wq = state_dict.pop(f'model.layers.{l}.self_attn.q_proj.weight')
+        Wk = state_dict.pop(f'model.layers.{l}.self_attn.k_proj.weight')
         Wq = unpermute(Wq)
         Wk = unpermute(Wk)
-        Wv = state_dict.pop(f'transformer.layers.{l}.self_attn.v_proj.weight')
-        state_dict[f'transformer.layers.{l}.mixer.Wqkv.weight'] = torch.cat([Wq, Wk, Wv], dim=0)
+        Wv = state_dict.pop(f'model.layers.{l}.self_attn.v_proj.weight')
+        state_dict[f'model.layers.{l}.mixer.Wqkv.weight'] = torch.cat([Wq, Wk, Wv], dim=0)
         # We don't store these
-        state_dict.pop(f'transformer.layers.{l}.attention.inner_attention.rope.freqs', None)
+        state_dict.pop(f'model.layers.{l}.attention.inner_attention.rope.freqs', None)
 
     def key_mapping_attn(key):
-        return re.sub(r'^transformer.layers.(\d+).self_attn.o_proj.',
-                      r'transformer.layers.\1.mixer.out_proj.', key)
+        return re.sub(r'^model.layers.(\d+).self_attn.o_proj.',
+                      r'model.layers.\1.mixer.out_proj.', key)
 
     state_dict = OrderedDict((key_mapping_attn(k), v) for k, v in state_dict.items())
 
     def key_mapping_rotary(key):
-        return re.sub(r'^transformer.layers.(\d+).self_attn.rotary_emb.inv_freq',
-                      r'transformer.layers.\1.mixer.rotary_emb.inv_freq', key)
+        return re.sub(r'^model.layers.(\d+).self_attn.rotary_emb.inv_freq',
+                      r'model.layers.\1.mixer.rotary_emb.inv_freq', key)
 
     state_dict = OrderedDict((key_mapping_rotary(k), v) for k, v in state_dict.items())
     return state_dict
